2945fac6b1e05f29c936acb9bb5caa83a7ac90bc
Jacob Killelea <Jkillelea@protonmail.ch>
2019-06-04 19:02:01 -0700

    output format tweak

--- main.c
+++ main.c
@@ -147,11 +147,11 @@ int main(int argc, char const *argv[]) {
         char lat_dir_ptr[2] = {lat_dir, '\0'};
         char lon_dir_ptr[2] = {lon_dir, '\0'};
 
-        printf("\n%2d matches. %2d sats, quality %d, time %.0lf, %lf %s, %lf %s\n",
-            nmatches, nsats, fix_qual, timestamp,
-            lat/100.0, lat_dir_ptr, lon/100.0, lon_dir_ptr);
-        printf("altitude: %.1lf (sl), %.1lf (wgs84), %lf (horiz_dil)\n",
-            alt_sl, alt_wgs84ellipsoid, horizontal_dilution);
+        printf("\n%2d sats, quality %d, time %.0lf, %lf %s, %lf %s\n",
+            nsats, fix_qual, timestamp, lat/100.0, lat_dir_ptr, 
+            lon/100.0, lon_dir_ptr);
+        printf("altitude: %.1lf (sl), %.1lf (wgs84), %lf (horiz_dil), %d matches\n",
+            alt_sl, alt_wgs84ellipsoid, horizontal_dilution, nmatches);
 
         memset(buf, 0, sizeof(buf));
         tcflush(fd, TCIOFLUSH);
